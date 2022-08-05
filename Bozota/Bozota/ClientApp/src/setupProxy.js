40daddf7825e461c820db21dfc0b743018e37017
veikko <veikko@lintujarvi.fi>
2022-08-05 20:49:00 +0300

    Config SPA proxy

--- Bozota/Bozota/ClientApp/src/setupProxy.js
+++ Bozota/Bozota/ClientApp/src/setupProxy.js
@@ -7,7 +7,7 @@ const target = env.ASPNETCORE_HTTPS_PORT
   ? env.ASPNETCORE_URLS.split(';')[0]
   : 'http://localhost:46370';
 
-const context = ['/game/status'];
+const context = ['/game/**'];
 
 module.exports = function (app) {
   const appProxy = createProxyMiddleware(context, {
