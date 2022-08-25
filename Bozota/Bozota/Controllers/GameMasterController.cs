953ce4664faea43997cbe608e1d4498266805415
Daniel Mäki <daniel.maki@if.fi>
2022-08-25 17:52:50 +0300

    Fixed naming in GameMasterController

--- Bozota/Bozota/Controllers/GameMasterController.cs
+++ Bozota/Bozota/Controllers/GameMasterController.cs
@@ -9,16 +9,16 @@ namespace Bozota.Controllers
     public class GameMasterController : ControllerBase
     {
         private readonly ILogger<GameMasterController> _logger;
-        private readonly GameMasterService _gameProgress;
+        private readonly GameMasterService _gameMaster;
 
         private bool _isGameInitializing = false;
         private bool _isGameUpdating = false;
 
         public GameMasterController(ILogger<GameMasterController> logger,
-            GameMasterService gameProgress)
+            GameMasterService gameMaster)
         {
             _logger = logger;
-            _gameProgress = gameProgress;
+            _gameMaster = gameMaster;
         }
 
         [HttpGet]
@@ -34,7 +34,7 @@ namespace Bozota.Controllers
                 if (!_isGameInitializing)
                 {
                     _isGameInitializing = true;
-                    gameMap = await _gameProgress.InitializeGameAsync();
+                    gameMap = await _gameMaster.InitializeGameAsync();
                 }
             }
             catch (Exception ex)
@@ -62,7 +62,7 @@ namespace Bozota.Controllers
                 if (!_isGameUpdating)
                 {
                     _isGameUpdating = true;
-                    gameMap = await _gameProgress.UpdateGameAsync();
+                    gameMap = await _gameMaster.UpdateGameAsync();
                 }
             }
             catch (Exception ex)
