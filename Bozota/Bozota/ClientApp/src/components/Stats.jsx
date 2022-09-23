2b0d739cade27df265267981439350bd5caf503c
veikko <veikko@lintujarvi.fi>
2022-09-23 21:53:10 +0300

    Improved stats

--- Bozota/Bozota/ClientApp/src/components/Stats.jsx
+++ Bozota/Bozota/ClientApp/src/components/Stats.jsx
@@ -1,20 +1,53 @@
 import React from 'react';
 import './Stats.css';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { GiAmmoBox, GiMetalBoot, GiWoodenCrate } from 'react-icons/gi';
+
+const renderCounters = (key, value, color) => {
+  const counters = [];
+  for (var i = 0; i < value; i++) {
+    counters.push(
+      <span
+        key={`${key}_${i}`}
+        className={`Stats_counter Stats_counter-${color}`}
+      />
+    );
+  }
+  return counters;
+};
 
 export const Stats = ({ gameState }) => {
   return (
-    <div>
+    <div className='Stats_playerContainer'>
       {gameState && (
         <>
           {gameState.players.map((player) => (
-            <div className='Stats_playerContainer' key={player.name}>
-              <h2>Name: {player.name}</h2>
-              <h2>
-                Health: {player.health.points} / {player.health.maxPoints}
+            <div className='Stats_player' key={player.name}>
+              <h2 className='Stats_stat Stats_playerName'>{player.name}</h2>
+              <h2 className='Stats_stat'>
+                <AiOutlineHeart />{' '}
+                <div className='Stats_counterContainer'>
+                  {renderCounters('health', player.health.points, 'red')}
+                </div>
               </h2>
-              <h2>Ammo: {player.ammo}</h2>
-              <h2>Speed: {player.speed}</h2>
-              <h2>Materials: {player.materials}</h2>
+              <h2 className='Stats_stat'>
+                <GiAmmoBox />
+                <div className='Stats_counterContainer'>
+                  {renderCounters('ammo', player.ammo, 'yellow')}
+                </div>
+              </h2>
+              <h2 className='Stats_stat'>
+                <GiMetalBoot />
+                <div className='Stats_counterContainer'>
+                  {renderCounters('speed', player.speed, 'blue')}
+                </div>
+              </h2>
+              <h2 className='Stats_stat'>
+                <GiWoodenCrate />
+                <div className='Stats_counterContainer'>
+                  {renderCounters('materials', player.materials, 'brown')}
+                </div>
+              </h2>
             </div>
           ))}
         </>
