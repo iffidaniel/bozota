f4a8da115c455099a8fc96f9dca6c2c560223efd
DianaRocks69 <dinciss@gmail.com>
2022-09-29 14:14:23 +0300

    Changed icon styles

--- Bozota/Bozota/ClientApp/src/components/GameMap.jsx
+++ Bozota/Bozota/ClientApp/src/components/GameMap.jsx
@@ -1,27 +1,58 @@
 import React from 'react';
 import './GameMap.css';
+import {
+  BiChevronDownSquare,
+  BiChevronUpSquare,
+  BiCaretRight,
+  BiError
+} from 'react-icons/bi';
+import {
+  FaHeartbeat,
+  FaBomb,
+  FaBattleNet,
+FaBahai
+} from 'react-icons/fa';
+import {
+  BsSuitHeartFill,
+  
+  
+} from 'react-icons/bs';
+import {
+  AiOutlineBuild,
+  AiOutlineFire,
+  AiTwotoneGold
+  
+} from 'react-icons/ai';
+import {
+  GiHeavyBullets,
+  GiBrickWall,
+  GiClusterBomb,
+  GiBulletImpacts,
+  GiClayBrick,
+  GiMineExplosion,
+} from 'react-icons/gi';
 
 const renderItem = (id, key) => {
   if (id === 0) {
     return <span className='GameMap_cell_empty GameMap_cell' key={key} />;
   } else if (id === 1) {
-    return <span className='GameMap_cell_health GameMap_cell' key={key} />;
+    return <BsSuitHeartFill className='GameMap_cell_health GameMap_cell' key={key} />;
   } else if (id === 2) {
-    return <span className='GameMap_cell_ammo GameMap_cell' key={key} />;
+    return <GiHeavyBullets className='GameMap_cell_ammo GameMap_cell' key={key} />;
   } else if (id === 3) {
-    return <span className='GameMap_cell_wall GameMap_cell' key={key} />;
+    return <GiBrickWall className='GameMap_cell_wall GameMap_cell' key={key} />;
   } else if (id === 4) {
-    return <span className='GameMap_cell_bomb GameMap_cell' key={key} />;
+    return <FaBomb className='GameMap_cell_bomb GameMap_cell' key={key} />;
   } else if (id === 5) {
     return <span className='GameMap_cell_player GameMap_cell' key={key} />;
   } else if (id === 6) {
-    return <span className='GameMap_cell_bullet GameMap_cell' key={key} />;
+    return <FaBahai className='GameMap_cell_bullet GameMap_cell' key={key} />;
   } else if (id === 7) {
-    return <span className='GameMap_cell_fire GameMap_cell' key={key} />;
+    return <GiMineExplosion className='GameMap_cell_fire GameMap_cell' key={key} />;
   } else if (id === 8) {
-    return <span className='GameMap_cell_materials GameMap_cell' key={key} />;
+    return <AiOutlineBuild className='GameMap_cell_materials GameMap_cell' key={key} />;
   } else {
-    return <span className='GameMap_cell_error GameMap_cell' key={key} />;
+    return <BiError className='GameMap_cell_error GameMap_cell' key={key} />;
   }
 };
 
