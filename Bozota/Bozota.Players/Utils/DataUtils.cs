7ec98f64842fac8d8301cd12d83c0c97be4d5e83
Veikko Lintujärvi <veikko.lintujarvi@if.fi>
2022-09-29 16:53:11 +0300

    New data utils

---
+++ Bozota/Bozota.Players/Utils/DataUtils.cs
@@ -0,0 +1,13 @@
+using Bozota.Common.Models;
+
+namespace Bozota.Players.Utils;
+public static class DataUtils
+{
+    public static int GetDistance(Position posA, Position posB)
+    {
+        var yDiff = posA.Y - posB.Y;
+        var xDiff = posA.X - posB.X;
+
+        return Math.Abs(yDiff) + Math.Abs(xDiff);
+    }
+}
