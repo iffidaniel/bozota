6a8be4dc477ef9821f8ee5dc440eb0624f0c5b30
Krishna <@>
2022-09-30 10:41:05 +0300

    initial console project

---
+++ Bozota/Bozota.Player.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Confluent.Kafka;
+
+
+var configBuilder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddIniFile("kafka.properties")
+    .AddJsonFile($"appsettings.json");
+
+var config = configBuilder.Build();
+var playerName = config.GetValue<string>("PlayerName");
+Console.WriteLine($"Connecting player {playerName}");
+
+
+var playerTopic = config.GetValue<string>("KafkaPlayersTopic");
+var gameStateTopic = config.GetValue<string>("KafkaGameStateTopic");
+
+
+using (var producer = new ProducerBuilder<string, string>(
+         config.AsEnumerable()).Build())
+{
+    Console.WriteLine("Trying to send message to ");
+    producer.Produce(playerTopic, new Message<string, string> { Key = "Player1", Value = "moveRight" },
+        (deliveryReport) =>
+        {
+            if (deliveryReport.Error.Code != ErrorCode.NoError)
+            {
+                Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+            }
+            else
+            {
+                Console.WriteLine($"Produced event to topic {playerTopic}");
+            }
+        });
+    producer.Flush(TimeSpan.FromSeconds(10));
+}
