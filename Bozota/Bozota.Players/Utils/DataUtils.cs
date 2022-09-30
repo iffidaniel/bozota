fd022f29f96fe70736735de5eff529ff2914a22a
Veikko Lintujärvi <veikko.lintujarvi@if.fi>
2022-09-30 17:03:16 +0300

    New data function: GetStraightPlayerDirection

--- Bozota/Bozota.Players/Utils/DataUtils.cs
+++ Bozota/Bozota.Players/Utils/DataUtils.cs
@@ -1,4 +1,5 @@
 using Bozota.Common.Models;
+using Bozota.Common.Models.Players;
 
 namespace Bozota.Players.Utils;
 
@@ -10,5 +11,41 @@ public static class DataUtils
         var xDiff = posA.X - posB.X;
 
         return Math.Abs(yDiff) + Math.Abs(xDiff);
+    }
+
+    /// <summary>
+    /// Calculates if there the player is on stright line vertically of horizontally
+    /// </summary>
+    /// <param name="from">Player to calculate from</param>
+    /// <param name="to">Player to calculate to</param>
+    /// <returns>Direction if player is on straigth line, Direction.None if none on the line</returns>
+    public static Direction GetStraigthPlayerDirection(Player from, Player to)
+    {
+        if (from.YPos == to.YPos)
+        {
+            if (from.XPos - to.XPos < 0)
+            {
+                return Direction.Left;
+            }
+            else
+            {
+                return Direction.Right;
+            }
+        }
+        else if (from.XPos == to.XPos)
+        {
+            if (from.YPos - to.YPos < 0)
+            {
+                return Direction.Down;
+            }
+            else
+            {
+                return Direction.Up;
+            }
+        }
+        else
+        {
+            return Direction.None;
+        }
     }
 }
