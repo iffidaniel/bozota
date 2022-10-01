f5c9db4d6b5505c4653caa56dbf3acab6311fbdb
Raif Hairulla <raif.hairulla@if.fi>
2022-10-01 23:55:34 +0300

    Updates to Raif Player

--- Bozota/Bozota.Players/Raif/Raif.cs
+++ Bozota/Bozota.Players/Raif/Raif.cs
@@ -1,6 +1,7 @@
 ﻿using Bozota.Common.Models;
 using Bozota.Common.Models.Items;
 using Bozota.Common.Models.Items.Abstractions;
+using Bozota.Common.Models.Objects.Abstractions;
 using Bozota.Common.Models.Players;
 using Bozota.Players.Utils;
 
@@ -9,24 +10,35 @@ namespace Bozota.Players.Raif;
 public class Raif : IPlayingPlayer
 {
     public string Name => "Raif";
+    private int _bombExplosionRadius = 5;
     private Player _me = new();
+    private GameState _gameState = new();
 
     private int _continuousShoots = 0;
     private Direction _shootDirection = Direction.None;
     private Position _previousPosition = new();
     private bool _previousActionWasMove = false;
+    private bool _previousShotBomb = false;
 
     public PlayerAction NextAction(GameStateUtils gameStateUtils)
     {
-        var state = gameStateUtils.GameState;
-        _me = state.Players.First(p => p.Name == Name);
+        _gameState = gameStateUtils.GameState;
+        _me = _gameState.Players.First(p => p.Name == Name);
 
         PlayerAction? playerAction = null;
         if (_me.Ammo > 0)
         {
-            // Shoot
-            playerAction = ActionUtils.ShootClosest(Name, state.Players);
-            if (_continuousShoots > 3 && _shootDirection == playerAction?.Direction)
+            playerAction = ActionUtils.ShootClosest(Name, _gameState.Players);
+            if (playerAction is null && !_previousShotBomb)
+            {
+                playerAction = ShootBomb();
+                _previousShotBomb = true;
+            }
+            else
+            {
+                _previousShotBomb = false;
+            }
+            if (_continuousShoots > 2 && _shootDirection == playerAction?.Direction)
             {
                 playerAction = null;
             }
@@ -38,7 +50,6 @@ public class Raif : IPlayingPlayer
         }
         if (playerAction is null)
         {
-            // Move
             ResetShoots();
 
             var myPosition = new Position { X = _me.XPos, Y = _me.YPos };
@@ -47,15 +58,24 @@ public class Raif : IPlayingPlayer
             int distHealth = health is null ? 1000 : DataUtils.GetDistance(myPosition, new Position { X = health.XPos, Y = health.YPos });
             int distAmmo = ammo is null ? 1000 : DataUtils.GetDistance(myPosition, new Position { X = ammo.XPos, Y = ammo.YPos });
 
-            if (_me.Health.Points <= 50 || distHealth <= distAmmo)
+            if (distHealth <= distAmmo)
                 playerAction = MoveTo(health);
             else
                 playerAction = MoveTo(ammo);
 
-            if (playerAction is null || _previousActionWasMove && _previousPosition.X == _me.XPos && _previousPosition.Y == _me.YPos)
-                playerAction = MoveRandomly(gameStateUtils);
+            if (_previousActionWasMove && _previousPosition.X == _me.XPos && _previousPosition.Y == _me.YPos)
+            {
+                // Shoot wall
+                playerAction = new PlayerAction(Name, GameAction.Shoot, playerAction.Direction);
+                _previousActionWasMove = false;
+            }
+            else
+                _previousActionWasMove = true;
 
-            _previousActionWasMove = true;
+            if (playerAction is null)
+            {
+                playerAction = RandomMove(gameStateUtils);
+            }
         }
         else
         {
@@ -74,56 +94,57 @@ public class Raif : IPlayingPlayer
         _shootDirection = Direction.None;
     }
 
-    private PlayerAction? MoveRandomly(GameStateUtils stateUtils)
+    private PlayerAction? RandomMove(GameStateUtils stateUtils)
     {
         int testDirection;
         Random r = new Random();    
         testDirection = r.Next(1, 4);
         return new PlayerAction(Name, GameAction.Move, (Direction)testDirection);
-        Direction chosenDirection = Direction.None;
 
-        for (int i = 0; i < 4; i++)
-        {
-            switch(testDirection)
-            {
-                case (int)Direction.Up:
-                    if (stateUtils.TakenPositions.Any(p => p.X == _me.XPos && p.Y == _me.YPos - 1))
-                    {
-                        chosenDirection = Direction.Up;
-                    }
-                    break;
-                case (int)Direction.Down:
-                    if (stateUtils.TakenPositions.Any(p => p.X == _me.XPos && p.Y == _me.YPos + 1))
-                    {
-                        chosenDirection = Direction.Down;
-                    }
-                    break;
-                case (int)Direction.Left:
-                    if (stateUtils.TakenPositions.Any(p => p.X == _me.XPos - 1 && p.Y == _me.YPos))
-                    {
-                        chosenDirection = Direction.Left;
-                    }
-                    break;
-                case (int)Direction.Right:
-                    if (stateUtils.TakenPositions.Any(p => p.X == _me.XPos + 1 && p.Y == _me.YPos))
-                    {
-                        chosenDirection = Direction.Right;
-                    }
-                    break;
-            }
-            if (chosenDirection == Direction.None)
-            {
-                if (testDirection == 4)
-                    testDirection = 1;
-                else
-                    testDirection++;
-            }
-            else
-            {
-                break;
-            }
-        }
-        return new PlayerAction(Name, GameAction.Move, chosenDirection);
+        //Direction chosenDirection = Direction.None;
+
+        //for (int i = 0; i < 4; i++)
+        //{
+        //    switch(testDirection)
+        //    {
+        //        case (int)Direction.Up:
+        //            if (stateUtils.TakenPositions.Any(p => p.X == _me.XPos && p.Y == _me.YPos - 1))
+        //            {
+        //                chosenDirection = Direction.Up;
+        //            }
+        //            break;
+        //        case (int)Direction.Down:
+        //            if (stateUtils.TakenPositions.Any(p => p.X == _me.XPos && p.Y == _me.YPos + 1))
+        //            {
+        //                chosenDirection = Direction.Down;
+        //            }
+        //            break;
+        //        case (int)Direction.Left:
+        //            if (stateUtils.TakenPositions.Any(p => p.X == _me.XPos - 1 && p.Y == _me.YPos))
+        //            {
+        //                chosenDirection = Direction.Left;
+        //            }
+        //            break;
+        //        case (int)Direction.Right:
+        //            if (stateUtils.TakenPositions.Any(p => p.X == _me.XPos + 1 && p.Y == _me.YPos))
+        //            {
+        //                chosenDirection = Direction.Right;
+        //            }
+        //            break;
+        //    }
+        //    if (chosenDirection == Direction.None)
+        //    {
+        //        if (testDirection == 4)
+        //            testDirection = 1;
+        //        else
+        //            testDirection++;
+        //    }
+        //    else
+        //    {
+        //        break;
+        //    }
+        //}
+        //return new PlayerAction(Name, GameAction.Move, chosenDirection);
     }
 
     private PlayerAction? MoveTo(IMapItem? mapItem)
@@ -149,6 +170,98 @@ public class Raif : IPlayingPlayer
             {
                 return new PlayerAction(Name, GameAction.Move, Direction.Down);
             }
+        }
+    }
+
+    private PlayerAction? ShootBomb()
+    {
+        PlayerAction? action = null;
+        BombMetaData bombToShoot = null;
+        var bombs = GetBombsInAxisSectors();
+        List<Direction> invalidDirections = new List<Direction>();
+
+        foreach (var b in bombs)
+        {
+            if (b.Distance <= _bombExplosionRadius)
+            {
+                if (!invalidDirections.Contains(b.Direction))
+                    invalidDirections.Add(b.Direction);
+            }
+        }
+
+        foreach (var b in bombs)
+        {
+            if (!invalidDirections.Contains(b.Direction) && b.Distance > _bombExplosionRadius && (bombToShoot is null || bombToShoot.Distance > b.Distance))
+                bombToShoot = b;
+        }
+
+        if (bombToShoot is not null)
+            action = new PlayerAction(Name, GameAction.Shoot, bombToShoot.Direction);
+        
+        return action;
+    }
+
+    private List<BombMetaData> GetBombsInAxisSectors()
+    {
+        List<BombMetaData> bombs = new List<BombMetaData>();
+        foreach (var b in _gameState.Bombs)
+        {
+            var distance = DataUtils.GetDistance(new Position { X = _me.XPos, Y = _me.YPos }, new Position { X = b.XPos, Y = b.YPos });
+            if (b.XPos == _me.XPos)
+            {
+                bombs.Add(
+                new BombMetaData(
+                    b.YPos > _me.YPos ? Direction.Up : Direction.Down,
+                    distance,
+                    b));
+            }
+            else if (b.YPos == _me.YPos)
+            {
+                bombs.Add(
+                new BombMetaData(
+                    b.XPos > _me.XPos ? Direction.Right : Direction.Left,
+                    distance,
+                    b));
+            }
+        }
+        return bombs;
+    }
+
+    //private IBombObject? FindClosestBomb(Position origin, GameState gameState)
+    //{
+    //    if (gameState.Bombs.Count <= 0)
+    //    {
+    //        return null;
+    //    }
+
+    //    IBombObject? itemWithLeastDistance = gameState.Bombs[0];
+    //    var leastDistance = DataUtils.GetDistance(origin, new Position { X = gameState.Bombs[0].XPos, Y = gameState.Bombs[0].YPos });
+    //    foreach (var item in gameState.Bombs)
+    //    {
+    //        var distance = DataUtils.GetDistance(origin, new Position { X = item.XPos, Y = item.YPos });
+    //        if (distance < leastDistance)
+    //        {
+    //            leastDistance = distance;
+    //            itemWithLeastDistance = item;
+    //        }
+    //    }
+
+    //    return itemWithLeastDistance;
+    //}
+
+    private class BombMetaData
+    {
+        public Direction Direction { get; set; }
+
+        public int Distance { get; set; }
+
+        public IBombObject BombObject { get; set; }
+
+        public BombMetaData(Direction direction, int distance, IBombObject bombObject)
+        {
+            Direction = direction;
+            Distance = distance;
+            BombObject = bombObject;
         }
     }
 
